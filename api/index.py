7088ac3de8ecec649f8130cfbedcb9258d49a731
paliman01 <163369665+paliman01@users.noreply.github.com>
2024-08-20 21:58:21 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,95 +1,40 @@
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
-import os
-import sys
-from argparse import ArgumentParser
-
 from flask import Flask, request, abort
-from linebot.v3 import (
-     WebhookHandler
+from flask_restful import Resource
+import json
+import os
+from linebot import (
+    LineBotApi, WebhookHandler
 )
-from linebot.v3.exceptions import (
+from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent,
-)
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
-
-app = Flask(__name__)
-
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('e8c2e45c74674cef947c02cc35b9d985', None)
-channel_access_token = os.getenv('5v7FYwYR94oMw/4KAGOt3GpdNiMF4T/HuC2GJpFPV06DibGcTjwaMSd0tM3TuTGUoHu+IcWbOqz4LqdjLaHTqKOJg5I7dfmfP6AtchYj5IPbk1mxsJ2f8ZS+mlKviD7y9sqEEM+mFnS71HG1SMGrLQdB04t89/1O/w1cDnyilFU=', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
-handler = WebhookHandler(channel_secret)
-
-configuration = Configuration(
-    access_token=channel_access_token
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+class LineMessageApiWebhookController(Resource):
+    def post(self):
+        line_bot_api = LineBotApi(os.getenv('5v7FYwYR94oMw/4KAGOt3GpdNiMF4T/HuC2GJpFPV06DibGcTjwaMSd0tM3TuTGUoHu+IcWbOqz4LqdjLaHTqKOJg5I7dfmfP6AtchYj5IPbk1mxsJ2f8ZS+mlKviD7y9sqEEM+mFnS71HG1SMGrLQdB04t89/1O/w1cDnyilFU='))
+        handler = WebhookHandler(os.getenv('e8c2e45c74674cef947c02cc35b9d985'))
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+        # get X-Line-Signature header value
+        signature = request.headers['X-Line-Signature']
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+        body = request.get_data(as_text=True)
+        event = json.loads(body)
+        print(event)
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            print(
+                "Invalid signature. Please check your channel access token/channel secret.")
+            abort(400)
 
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessageContent)
-def message_text(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
-
-
-if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+        token = event['events'][0]['replyToken']
+        if token == "00000000000000000000000000000000":
+            pass
+        else:
+            line_bot_api.reply_message(token, TextSendMessage(
+                text=event['events'][0]['message']['text']))
+        return 'OK'
